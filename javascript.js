938e5e98cd2f3971de672b3e6ad5de12a0413559
sleepytetris <seonjae912@gmail.com>
2022-06-04 16:08:24 +0100

    Add playRound() function that plays a single round of RPS

--- javascript.js
+++ javascript.js
@@ -3,7 +3,32 @@ function computerPlay() { //randomly returns Rock, Paper, or Scissors.
     return choice[Math.floor(Math.random()*3)];
 }
 
-function round(playerSelecion, computerSelection) { //plays a single round of RPS, and returns a string declaring the winner
-
+function capitalise(word) { //capitalises only the first letter of a word
+    firstLetter = word[0].toUpperCase();
+    rest = word.slice(1).toLowerCase();
+    return firstLetter + rest;
 }
 
+function playRound(playerSelection, computerSelection) { //plays a single round of RPS, and returns a string declaring the winner
+    let casePlayerSelection = capitalise(playerSelection); //makes playerSelection case insensitive
+
+    if (casePlayerSelection === computerSelection) {
+        return "It's a Tie! You both chose " + computerSelection;
+    }
+    else if (casePlayerSelection == "Rock") {
+        playerWin = (computerSelection == "Scissors");
+    }
+    else if (casePlayerSelection == "Scissors") {
+        playerWin = (computerSelection == "Paper");
+    }
+    else if (casePlayerSelection == "Paper") {
+        playerWin = (computerSelection == "Rock");
+    }
+
+    if (playerWin) {
+        return "You Win! " + casePlayerSelection + " beats " + computerSelection;
+    }
+    else {
+        return "You Lose! " + computerSelection + " beats " + casePlayerSelection;
+    }
+}
